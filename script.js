dbf7958bea0323337e6e017ae71e36063b286044
Prathamesh Patil <prathameshpatil3018@gmail.com>
2023-10-19 17:22:14 +0530

    updated

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+  const sec2img = document.getElementsByClassName('sec2img');
+
+  const sec2card = document.getElementsByClassName('sec2-card');
+
+   
+sec2card.addEventListner('mouseover', () => {
+
+  sec2img
+})
+  
